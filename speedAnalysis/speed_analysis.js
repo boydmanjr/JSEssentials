66db725042ec9f46d2c87be47f576f784c4d1430
Rob Boyd <boydmanjr@att.net>
2026-02-21 10:45:20 -0500

    added total length to speed analysis app

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -32,6 +32,8 @@ function endTest() {
         return word !== "";
     }).length;
 
+    var total_Length = userTypedText.length;
+
     var wpm = 0; // Default value
 
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
@@ -41,6 +43,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + total_Length + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
